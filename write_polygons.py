6074a0b9127df9ea2918fd43344ba04c206fb2d1
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-08-10 18:40:27 +0300

    Fix get_bbox,
    -    chained = chain(*polygons)
    -    x1 = min(x for x, _ in chained)
    ...
    +    x1 = min(x for x, _ in chain(*polygons))
    
    Add __iter__ to Point, Bbox
    
    Add __repr__ to Header

--- write_polygons.py
+++ write_polygons.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
 from itertools import chain
-from beartype import beartype
+from typeguard import typechecked
 
 
 POLYGONS = [
@@ -13,35 +13,44 @@ POLYGONS = [
 
 
 class Point:
-    @beartype
+    @typechecked
     def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
 
+    def __iter__(self):
+        yield from self.__dict__.values()
+
 
 class Bbox:
-    @beartype
+    @typechecked
     def __init__(self, xy1: Point, xy2: Point):
         self.xy1 = xy1
         self.xy2 = xy2
 
+    def __iter__(self):
+        yield from self.__dict__.values()
+
 
 def get_bbox(polygons=POLYGONS) -> Bbox:
-    chained = chain(*polygons)
-    x1 = min(x for x, _ in chained)
-    y1 = min(y for _, y in chained)
-    x2 = max(x for x, _ in chained)
-    y2 = max(y for _, y in chained)
+    x1 = min(x for x, _ in chain(*polygons))
+    y1 = min(y for _, y in chain(*polygons))
+    x2 = max(x for x, _ in chain(*polygons))
+    y2 = max(y for _, y in chain(*polygons))
     return Bbox(Point(x1, y1), Point(x2, y2))
 
 
 class Header:
     def __init__(self, polygons=POLYGONS):
         self.magic = 0x1234
-        bbox = Bbox(polygons)
+        bbox = get_bbox(polygons)
         self.x1, self.y1 = bbox.xy1
         self.x2, self.y2 = bbox.xy2
         self.num_polygons = 3
+
+    def __repr__(self):
+        args = ", ".join(f"{k}={v!r}" for k, v in self.__dict__.items())
+        return f"{type(self).__name__}({args})"
 
 
 if __name__ == "__main__":
