b5f18e38c331f2397a9416e49040aa328dd2bc8c
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-08-16 20:26:00 +0300

    Break long string lines in test_header()

--- tests/test_view_constructors.py
+++ tests/test_view_constructors.py
@@ -51,9 +51,30 @@ def test_bbox_no_attribue():
     [
         (
             Header(),
-            "Header(magic=0, bbox=Bbox(xy1=Point(x=0.0, y=0.0), xy2=Point(x=0.0, y=0.0)), num_polygons=0)",
+            (
+                "Header(magic=0, bbox=Bbox(xy1=Point(x=0.0, y=0.0), "
+                "xy2=Point(x=0.0, y=0.0)), num_polygons=0)"
+            ),
+        ),
+        (
+            Header.from_kwargs(
+                magic=0x1234,
+                bbox=Bbox.from_kwargs(
+                    xy1=Point.from_kwargs(x=1, y=2), xy2=Point.from_kwargs(x=10, y=20)
+                ),
+                num_polygons=3,
+            ),
+            (
+                "Header(magic=4660, bbox=Bbox(xy1=Point(x=1.0, y=2.0), "
+                "xy2=Point(x=10.0, y=20.0)), num_polygons=3)"
+            ),
         ),
     ],
 )
 def test_header(h, expected):
     assert str(h) == expected
+
+
+def test_header_no_attribute():
+    with pytest.raises(TypeError, match=re.escape("'Header' has no attribute(s) code")):
+        Header.from_kwargs(code=0x1234, num_polygons=3)
