167c926550d0e798e5dce6b8e576940f0d349891
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-08-17 10:15:52 +0300

    Refactor __call__
    -    _instances = {}
    +    _instances = defaultdict(dict)

--- singleton.py
+++ singleton.py
@@ -1,27 +1,34 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+from collections import defaultdict
 
 
 class SingletonMeta(type):
-    _instances = {}
+    _instances = defaultdict(dict)
 
     def __call__(cls, *args, **kwargs):
-        if cls not in type(cls)._instances:
-            type(cls)._instances[cls] = super().__call__(*args, **kwargs)
+        name = args[0]
+        instances = type(cls)._instances
+        if cls not in instances or name not in instances[cls]:
+            instances[cls][name] = super().__call__(*args, **kwargs)
         return type(cls)._instances[cls]
 
 
 class Module(metaclass=SingletonMeta):
-    def __init__(self):
-        print(f"Initializing {type(self).__name__}")
+    def __init__(self, name):
+        print(f"Initializing {type(self).__name__}({name})")
 
 
 class Logger(metaclass=SingletonMeta):
-    pass
+    def __init__(self, name):
+        print(f"Initializing {type(self).__name__}({name})")
 
 
 if __name__ == "__main__":
-    m1 = Module()
-    m2 = Module()
+    m1 = Module("functools")
+    m2 = Module("functools")
+    g1 = Logger("stream")
+    g2 = Logger("stream")
     assert m1 is m2
+    assert g1 is g2
