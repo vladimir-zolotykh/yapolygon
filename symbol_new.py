054784c23236fa7544a100c913e5de44fd23b423
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-08-18 09:05:54 +0300

    Refactor Symbol.__new__
    -    def __new__(cls, *args, **kwargs):
    -        name = args[0]
    +    def __new__(cls, name, pat):

--- symbol_new.py
+++ symbol_new.py
@@ -6,17 +6,13 @@
 class Symbol:
     _instances = {}
 
-    def __new__(cls, *args, **kwargs):
-        name = args[0]
+    def __new__(cls, name, pat):
         if name not in cls._instances:
-            cls._instances[name] = super().__new__(cls)
+            sym = cls._instances[name] = super().__new__(cls)
+            sym.name = name
+            sym.pat = pat
+            print(f"Initializing {cls.__name__}({name})")
         return cls._instances[name]
-
-    def __init__(self, name, pat):
-        if not hasattr(self, "name"):
-            print(f"Initializing {type(self).__name__}({name})")
-            self.name = name
-            self.pat = pat
 
     @classmethod
     def masterpat(cls):
